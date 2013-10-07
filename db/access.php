cb852daf8038817d610578764d03a085b4b2111b
3i Logic LMS <azmat@3ilogic.com>
2013-10-07 17:08:20 +0500

    Added new capability

--- db/access.php
+++ db/access.php
@@ -6,10 +6,11 @@ $capabilities = array(
     'block/learning_plan:viewpages' => array(
 
         'captype' => 'read',
-        'contextlevel' => CONTEXT_COURSE,
+        'contextlevel' => CONTEXT_BLOCK,
         'legacy' => array(
             'guest' => CAP_PREVENT,
             'student' => CAP_ALLOW,
+            'user' => CAP_ALLOW,
             'teacher' => CAP_ALLOW,
             'editingteacher' => CAP_ALLOW,
             'coursecreator' => CAP_ALLOW,
@@ -31,7 +32,7 @@ $capabilities = array(
     'block/learning_plan:managepages' => array(
 
         'captype' => 'read',
-        'contextlevel' => CONTEXT_COURSE,
+        'contextlevel' => CONTEXT_BLOCK,
         'legacy' => array(
             'guest' => CAP_PREVENT,
             'student' => CAP_PREVENT,
